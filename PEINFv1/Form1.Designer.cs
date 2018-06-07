dee1e126fb960e689296c3c58609f65524dec497
JakobFromGithub <38981027+JakobFromGithub@users.noreply.github.com>
2018-06-07 16:39:40 +0200

    Erde dreht sich

--- PEINFv1/Form1.Designer.cs
+++ PEINFv1/Form1.Designer.cs
@@ -28,20 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkCursorPosition = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkCursorPosition
+            // 
+            this.checkCursorPosition.Interval = 200;
+            this.checkCursorPosition.Tick += new System.EventHandler(this.checkCursorPosition_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(776, 492);
+            this.ClientSize = new System.Drawing.Size(800, 598);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer checkCursorPosition;
     }
 }
 
--- PEINFv1/Form1.cs
+++ PEINFv1/Form1.cs
@@ -13,16 +13,102 @@ using System.Windows.Forms;
 //  PEINFv1
 //
 //  von Tim Nikita und Köbi
-//
+
+
+#region TODO
+
+    //////////////////// 
+    //      TODO      //
+    ////////////////////
+        
+    // Links relativ machen Formload 
+
+#endregion
+//TODO RELATIVE LINK
+
 
 
 namespace PEINFv1
 {
     public partial class Form1 : Form
     {
+
+        #region Variablen
+
+        int currentFrame = 1;
+
+        Bitmap[] frames = new Bitmap[25];
+
+        #endregion
+
+        #region Events
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Cursor.Position.X.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 24; i++)
+            {
+                frames[i] = new Bitmap("..\\..\\Assets\\Erde\\frame (" + i + ").bmp"); 
+            }
+
+            Random rnd = new Random();
+            currentFrame = rnd.Next(24);
+
+            TurnEarth(true);
+            checkCursorPosition.Start();            
+        }
+
+        private void checkCursorPosition_Tick(object sender, EventArgs e)
+        {
+            if (Cursor.Position.X - Location.X > 600)
+            {
+                TurnEarth(false);
+            }
+
+            if (Cursor.Position.X - Location.X < 200)
+            {
+                TurnEarth(true);
+            }
+        }
+
+        #endregion
+
+        #region Methoden
+
+        private void TurnEarth(bool turnRight)
+        {
+            if (turnRight)
+            {
+                currentFrame++;
+            }
+            else
+            {
+                currentFrame--;
+            }
+
+            if (currentFrame > 24)
+            {
+                currentFrame = 1;
+            }
+
+            if (currentFrame < 1)
+            {
+                currentFrame = 24;
+            }
+
+            pictureBox1.Image = frames[currentFrame];
+        }
+
+        #endregion
+
+
     }
 }
