b0b31f6aba6235ae96ef4b6dae27bcd9e9582153
JakobFromGithub <38981027+JakobFromGithub@users.noreply.github.com>
2018-06-14 11:38:55 +0200

    Bugged Point added

--- PEINFv1/Form1.Designer.cs
+++ PEINFv1/Form1.Designer.cs
@@ -29,43 +29,61 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.checkCursorPosition = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.TempTextbox = new System.Windows.Forms.TextBox();
+            this.Point1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Point1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // checkCursorPosition
             // 
             this.checkCursorPosition.Interval = 200;
             this.checkCursorPosition.Tick += new System.EventHandler(this.checkCursorPosition_Tick);
             // 
+            // TempTextbox
+            // 
+            this.TempTextbox.Enabled = false;
+            this.TempTextbox.Location = new System.Drawing.Point(700, 580);
+            this.TempTextbox.Name = "TempTextbox";
+            this.TempTextbox.ReadOnly = true;
+            this.TempTextbox.Size = new System.Drawing.Size(100, 20);
+            this.TempTextbox.TabIndex = 1;
+            // 
+            // Point1
+            // 
+            this.Point1.BackColor = System.Drawing.Color.Transparent;
+            this.Point1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Point1.BackgroundImage")));
+            this.Point1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Point1.Location = new System.Drawing.Point(0, 0);
+            this.Point1.Name = "Point1";
+            this.Point1.Size = new System.Drawing.Size(20, 40);
+            this.Point1.TabIndex = 2;
+            this.Point1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 598);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Point1);
+            this.Controls.Add(this.TempTextbox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Point1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer checkCursorPosition;
+        private System.Windows.Forms.TextBox TempTextbox;
+        private System.Windows.Forms.PictureBox Point1;
     }
 }
 
--- PEINFv1/Form1.cs
+++ PEINFv1/Form1.cs
@@ -22,6 +22,10 @@ using System.Windows.Forms;
     ////////////////////
         
     // Punkte
+
+    //Nur drehen, wenn die Erde angewählt
+    
+     
 #endregion
 
 
@@ -35,11 +39,17 @@ namespace PEINFv1
 
         int currentFrame = 1;
 
+        Bitmap Pointer = new Bitmap("..\\..\\Assets\\Sonnst\\Point.png");
         Bitmap[] frames = new Bitmap[25];
+
+
+        //pointLocation[Nummer des Pointer, Frame, X = 0 oder Y = 1]
+        int[,,] pointLocation = new int[2,25, 2];
 
         #endregion
 
         #region Events
+
         public Form1()
         {
             InitializeComponent();
@@ -52,16 +62,49 @@ namespace PEINFv1
                 frames[i] = new Bitmap("..\\..\\Assets\\Erde\\frame (" + i + ").bmp"); 
             }
 
+            Point1.Image = Pointer;
+
             Random rnd = new Random();
             currentFrame = rnd.Next(24);
 
             TurnEarth(true);
-            checkCursorPosition.Start();            
+            checkCursorPosition.Start();
+
+            for (int i = 0; i < pointLocation.GetLength(0); i++)
+            {
+                for (int i2 = 0; i2 < pointLocation.GetLength(1); i2++)
+                {
+                    for (int i3 = 0; i3 < pointLocation.GetLength(2); i3++)
+                    {
+                        pointLocation[i, i2, i3] = 0;
+                    }
+                }
+            }
+
+            //Point 0
+
+            for (int i = 0; i < pointLocation.GetLength(1); i++)
+            {
+                pointLocation[0, i, 1] = 290;
+            }
+
+            pointLocation[0, 12, 0] = 26;
+            pointLocation[0, 13, 0] = 67;
+            pointLocation[0, 14, 0] = 132;
+            pointLocation[0, 15, 0] = 214;
+            pointLocation[0, 16, 0] = 311;
+            pointLocation[0, 17, 0] = 413;
+            pointLocation[0, 18, 0] = 517;
+            pointLocation[0, 19, 0] = 611;
+            pointLocation[0, 20, 0] = 755;
+            pointLocation[0, 21, 0] = 792;
+
+
         }
 
         private void checkCursorPosition_Tick(object sender, EventArgs e)
         {
-            if (Cursor.Position.X - Location.X > 600 && Cursor.Position.X > Size.Width)
+            /*if (Cursor.Position.X - Location.X > 600)//&& Cursor.Position.X > Size.Width
             {
                 TurnEarth(false);
             }
@@ -69,7 +112,9 @@ namespace PEINFv1
             if (Cursor.Position.X - Location.X < 200)//&& Cursor.Position.X < Size.Width
             {
                 TurnEarth(true);
-            }
+            }*/
+
+            TempTextbox.Text =   (Cursor.Position.X - this.Location.X).ToString() + "   " + (Cursor.Position.Y - this.Location.Y).ToString() + "   " + currentFrame.ToString();
         }
 
         #endregion
@@ -97,10 +142,35 @@ namespace PEINFv1
                 currentFrame = 24;
             }
 
-            pictureBox1.Image = frames[currentFrame];
+            this.BackgroundImage = frames[currentFrame];
+
+            if (pointLocation[0, currentFrame, 0] == 0)
+            {
+                Point1.Visible = false;
+            }
+            else
+            {
+                Point1.Visible = true;
+            }
+
+            Point1.Location = new Point(pointLocation[0, currentFrame, 0] + 60 , pointLocation[0, currentFrame, 1] -60);
+
         }
 
         #endregion
 
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    TurnEarth(true);
+                    break;
+
+                case Keys.Right:
+                    TurnEarth(false);
+                    break;
+            }
+        }
     }
 }
